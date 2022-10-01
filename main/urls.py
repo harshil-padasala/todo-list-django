11081f415a433a673824b5353a0077d29361a323
harshil <harshilpadsala999@gmail.com>
2022-10-01 19:25:54 +0530

    added like restricted pages, user specific data and user registration page

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
-    path("logout/", LogoutView.as_view(next_page='login'), name="logout"),
+    path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path("register/", RegisterPage.as_view(), name="register"),
     path('', TaskList.as_view(), name="task-list"), # ---> url for displaying task list page.
     path('task/<int:pk>', TaskDetail.as_view(), name='task'), # ---> url for displaying detail of task.
     path('task-create', TaskCreate.as_view(), name='task-create'), # ---> url for form of creating task.
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
-from .models import Task
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from .models import Task
 
 # Create your views here.
 
@@ -20,14 +25,39 @@ class CustomLoginView(LoginView):
         return reverse_lazy('task-list')
 
 
-class TaskList(ListView):
+class RegisterPage(FormView):
+    template_name = 'main/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('task-list')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user) 
+        return super(RegisterPage, self).form_valid(form)
+
+    # this method is used for block authenticated user to do not register again.
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('task-list')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+
+class TaskList(LoginRequiredMixin,ListView):
     """
     This class is used for displaying list of Task using ListView.
     """
     model = Task 
     context_object_name = 'tasks'
 
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(is_complete=False).count()
+        return context
+
+class TaskDetail(LoginRequiredMixin,DetailView):
     """
     This class is used for displaying details of particular task using DetailView.
     """
@@ -35,23 +65,27 @@ class TaskDetail(DetailView):
     context_object_name = 'task'
     template_name = 'main/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     """
     This class is used for creating a new task.
     """
     model = Task
-    fields = '__all__'
+    fields = ['task_title', 'description', 'is_complete']
     success_url = reverse_lazy('task-list')
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
+
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     """
     This class is used for updating a task.
     """
     model = Task
-    fields = '__all__'
+    fields = ['task_title', 'description', 'is_complete']
     success_url = reverse_lazy('task-list')
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     """
     This class is used for deleting a task.
     """
