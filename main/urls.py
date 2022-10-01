2531e087f04b8d600f59c80e5f4a84f93d43ea95
harshil <harshilpadsala999@gmail.com>
2022-10-01 15:33:00 +0530

    created create task page and update task_list page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TaskList, Task
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="task list"),
-    path('task/<int:pk>', Task.as_view(), name='task')
+    path('', TaskList.as_view(), name="task-list"), # ---> url for displaying task list page.
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'), # ---> url for displaying detail of task.
+    path('task-create', TaskCreate.as_view(), name='task-create'), # ---> url for form of creating task.
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 # Create your views here.
 
 class TaskList(ListView):
+    """
+    This class is used for displaying list of Task using ListView.
+    """
     model = Task 
     context_object_name = 'tasks'
 
-class Task(DetailView):
+class TaskDetail(DetailView):
+    """
+    This class is used for displaying details of particular task using DetailView.
+    """
     model = Task
     context_object_name = 'task'
     template_name = 'main/task.html'
+
+class TaskCreate(CreateView):
+    """
+    This class is used for creating a new task.
+    """
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('task-list')
