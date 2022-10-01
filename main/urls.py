4fdb126701371ce222d43e58bd7850835ba3181b
harshil <harshilpadsala999@gmail.com>
2022-10-01 14:36:08 +0530

    added task page to display details of task and update task_list page

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, Task
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task list"),
+    path('task/<int:pk>', Task.as_view(), name='task')
 ]
